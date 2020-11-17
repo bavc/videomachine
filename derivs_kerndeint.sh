f9d7b4936647f38843d99d43c1f8cdf0d12bd475
preservation <preservation@prez3-macmini.local>
2020-11-16 16:18:16 -0800

    changed kerndeint to make access and mezzanine

--- derivs_kerndeint.sh
+++ derivs_kerndeint.sh
@@ -14,7 +14,7 @@ $(basename "${0}")
    $(basename "${0}") MOVIE_FILE.mov
 
   Explanation:
-    This script will create a BAVC Standard Access File using the kerndeint filter to deinterliace
+    This script will create a BAVC Standard Access File and Deinterlad Mezzanine File using the kerndeint filter to deinterliace
 
   Options
    -h  display this help
@@ -42,7 +42,7 @@ else
     exit
 fi
 
-ffmpeg -vsync 0 -i "${1}" -c:v libx264 -pix_fmt yuv420p -movflags faststart -crf 18 -b:a 160000 -ar 48000 -aspect 4:3 -s 640x480 -vf setdar=4/3,crop=720:480:0:4,kerndeint "${1%.*}_access_kerndeint.mp4"
+ffmpeg -vsync 0 -i "${1}" -c:v libx264 -pix_fmt yuv420p -movflags faststart -crf 18 -b:a 160000 -ar 48000 -aspect 4:3 -s 640x480 -vf setdar=4/3,crop=720:480:0:4,kerndeint "${1%.*}_access_kerndeint.mp4" -c:v prores -profile:v 3 -vf kerndeint -c:a pcm_s24le "${1%.*}_mezzanine_kerndeint.mov"
 printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
-printf "ffmpeg -vsync 0 -i '${1}' -c:v libx264 -pix_fmt yuv420p -movflags faststart -crf 18 -b:a 160000 -ar 48000 -aspect 4:3 -s 640x480 -vf setdar=4/3,crop=720:480:0:4,kerndeint '${1%.*}_access_kerndeint.mp4' \n" >&2
+printf "ffmpeg -vsync 0 -i '${1}' -c:v libx264 -pix_fmt yuv420p -movflags faststart -crf 18 -b:a 160000 -ar 48000 -aspect 4:3 -s 640x480 -vf setdar=4/3,crop=720:480:0:4,kerndeint '${1%.*}_access_kerndeint.mp4' -c:v prores -profile:v 3 -vf kerndeint -c:a pcm_s24le '${1%.*}_mezzanine_kerndeint.mov' \n" >&2
 printf "********END FFMPEG COMMANDS********\n\n " >&2
