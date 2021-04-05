bc8e93dc6ea11c1e17ce8b7d228f66e5e68a7630
preservation <preservation@prez2-macmini.local>
2021-04-05 16:30:36 -0700

    fixed bugs for spaces in files names. added support for DV and FFV1

--- trimmer.sh
+++ trimmer.sh
@@ -87,44 +87,57 @@ fi
 
 filename=$(basename -- "${1}")
 extension="${filename##*.}"
-colour_primaries=$(mediainfo "--Output=Video;%colour_primaries%" ${1})
-transfer_characteristics=$(mediainfo "--Output=Video;%transfer_characteristics%" ${1})
-matrix_coefficients=$(mediainfo "--Output=Video;%matrix_coefficients%" ${1})
+colour_primaries=$(mediainfo "--Output=Video;%colour_primaries%" "${1}")
+transfer_characteristics=$(mediainfo "--Output=Video;%transfer_characteristics%" "${1}")
+matrix_coefficients=$(mediainfo "--Output=Video;%matrix_coefficients%" "${1}")
+file_format=$(mediainfo "--Output=Video;%Format/String%" "${1}")
+
+if [[ ${file_format} = "YUV" ]] ; then
+    mov_flags="-movflags write_colr -color_range mpeg"
+    printf "Inserting mov flag: -movflags write_colr -color_range mpeg \n" >&2
+fi
+
 
 if [[ ${colour_primaries} = "BT.601 NTSC" ]] ; then
-    color_primaries_flag="smpte170m"
-    printf "Color Primaries: ${color_primaries_flag} \n" >&2
+    color_primaries_flag="-color_primaries smpte170m"
+    printf "Color Primaries: BT.601 NTSC \n" >&2
 elif [[ ${colour_primaries} = "BT.709" ]] ; then
-    color_primaries_flag="bt709"
-    printf "Color Primaries: ${color_primaries_flag} \n" >&2
+    color_primaries_flag="-color_primaries  bt709"
+    printf "Color Primaries: BT.709 \n" >&2
+elif [[ ${file_format} = "DV" ]] ; then
+    printf "Color Primaries: DV, ignoring \n" >&2
 else
     printf "ERROR: Could not determine Colour Primaries of original file \n" >&2
     exit
 fi
 
 if [[ ${transfer_characteristics} = "BT.601" ]] ; then
-    transfer_characteristics_flag="smpte170m"
-    printf "Transfer Characteristics: ${transfer_characteristics_flag} \n" >&2
+    transfer_characteristics_flag="-color_trc smpte170m"
+    printf "Transfer Characteristics: BT.601 \n" >&2
 elif [[ ${transfer_characteristics} = "BT.709" ]] ; then
-    transfer_characteristics_flag="bt709"
-    printf "Transfer Characteristics: ${transfer_characteristics_flag} \n" >&2
+    transfer_characteristics_flag="-color_trc bt709"
+    printf "Transfer Characteristics: BT.709 \n" >&2
+elif [[ ${file_format} = "DV" ]] ; then
+    printf "Color Primaries: DV, ignoring \n" >&2
 else
     printf "ERROR: Could not determine Colour Primaries of original file \n" >&2
     exit
 fi
 
 if [[ ${matrix_coefficients} = "BT.601" ]] ; then
-    matrix_coefficients_flag="smpte170m"
-    printf "Matrix Coefficients: ${matrix_coefficients_flag} \n" >&2
+    matrix_coefficients_flag="-colorspace smpte170m"
+    printf "Matrix Coefficients: BT.601 \n" >&2
 elif [[ ${matrix_coefficients} = "BT.709" ]] ; then
-    matrix_coefficients_flag="bt709"
-    printf "Matrix Coefficients: ${matrix_coefficients_flag} \n" >&2
+    matrix_coefficients_flag="-colorspace bt709"
+    printf "Matrix Coefficients: BT.709 \n" >&2
+  elif [[ ${file_format} = "DV" ]] ; then
+      printf "Color Primaries: DV, ignoring \n" >&2
 else
     printf "ERROR: Could not determine Colour Primaries of original file \n" >&2
     exit
 fi
 
-ffmpeg -ss ${2} -to ${3} -i "${1}" -movflags write_colr -color_range mpeg -color_primaries ${color_primaries_flag} -color_trc ${transfer_characteristics_flag} -colorspace ${matrix_coefficients_flag} -c copy "${1%.*}_trimmed.${extension}"
+ffmpeg -ss ${2} -to ${3} -i "${1}" ${mov_flags} ${color_primaries_flag} ${transfer_characteristics_flag} ${matrix_coefficients_flag} -c copy "${1%.*}_trimmed.${extension}"
 printf "\n\n*******START FFMPEG COMMANDS*******\n" >&2
-printf "ffmpeg -ss ${2} -to ${3} -i '${1}' -movflags write_colr -color_range mpeg -color_primaries ${color_primaries_flag} -color_trc ${transfer_characteristics_flag} -colorspace ${matrix_coefficients_flag} -c copy '${1%.*}_trimmed.${extension}' \n" >&2
+printf "ffmpeg -ss ${2} -to ${3} -i '${1}' ${mov_flags} ${color_primaries_flag} ${transfer_characteristics_flag} ${matrix_coefficients_flag} -c copy '${1%.*}_trimmed.${extension}' \n" >&2
 printf "********END FFMPEG COMMANDS********\n\n " >&2
