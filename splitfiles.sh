c2d70f10fe8da602940a55a327d50e43e56f7685
preservation <preservation@prez3-macmini.local>
2021-06-22 14:20:04 -0700

    script is currently frame accurate and totally lossless when used with FFV1/MKV

--- splitfiles.sh
+++ splitfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Short script to split videos by filesize using ffmpeg by LukeLR (take from stack overflow: https://stackoverflow.com/questions/38259544/using-ffmpeg-to-split-video-files-by-size)
+# Short script to split videos by filesize using ffmpeg by LukeLR (taken from stack overflow: https://stackoverflow.com/questions/38259544/using-ffmpeg-to-split-video-files-by-size)
+# The BAVC version has been updated to be frame accurate. It was tested on 2021-06-22 to be completely frame accurate using an FFV1/MKV file
+# This script currently only works losslessly with MKV files, it needs some updates to properly handle color metadata for MOV
 
 if [ $# -ne 2 ]; then
     echo 'Illegal number of parameters. Needs at least 3 parameters:'
@@ -26,7 +28,7 @@ FILE="$1"
 SIZELIMIT="$2"
 
 # Duration of the source video
-DURATION=$(ffprobe -i "$FILE" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1|cut -d. -f1)
+DURATION=$(ffprobe -i "$FILE" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1)
 
 # Duration that has been encoded so far
 CUR_DURATION=0
@@ -47,16 +49,17 @@ NEXTFILENAME="$BASENAME-$i.$EXTENSION"
 echo "Duration of source video: $DURATION"
 
 # Until the duration of all partial videos has reached the duration of the source video
-while [[ $CUR_DURATION -lt $DURATION ]]; do
+while [[ $CUR_DURATION != $DURATION ]]; do
     # Encode next part
     echo ffmpeg -i "$FILE" -ss "$CUR_DURATION" -fs "$SIZELIMIT" $FFMPEG_ARGS "$NEXTFILENAME"
     ffmpeg -ss "$CUR_DURATION" -i "$FILE" -fs "$SIZELIMIT" $FFMPEG_ARGS "$NEXTFILENAME"
 
     # Duration of the new part
-    NEW_DURATION=$(ffprobe -i "$NEXTFILENAME" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1|cut -d. -f1)
+    NEW_DURATION=$(ffprobe -i "$NEXTFILENAME" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1)
 
     # Total duration encoded so far
-    CUR_DURATION=$((CUR_DURATION + NEW_DURATION))
+    CUR_DURATION=$( echo "$CUR_DURATION + $NEW_DURATION" | bc )
+    #CUR_DURATION=$((CUR_DURATION + NEW_DURATION))
 
     i=$((i + 1))
 
