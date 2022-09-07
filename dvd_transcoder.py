f7af9597a5632dc522ee0c232fc77ecc4d2819ca
iamdamosuzuki <morgan@bavc.org>
2022-09-06 17:25:00 -0700

    better keyboard exit handling

--- dvd_transcoder.py
+++ dvd_transcoder.py
@@ -133,6 +133,15 @@ def main():
         print bcolors.FAIL + "User has quit the script" + bcolors.ENDC
         try:
             unmount_Image(mount_point)
+            for the_file in os.listdir(args.i + ".VOBS"):
+                file_path = os.path.join(args.i + ".VOBS", the_file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+                except Exception as e:
+                    print(e)
+            os.rmdir(args.i + ".VOBS")
         except:
             pass
         quit()
